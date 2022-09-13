6e742816b4ea573bd245dd8909a52ca8c516bbc1
Rasanga Lakshith <64822421+rasangalk@users.noreply.github.com>
2022-09-13 21:18:51 +0530

    Create eslint environment

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,7 @@
-import Head from "next/head";
-import Image from "next/image";
+const Home = () => (
+  <div>
+    <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  </div>
+);
 
-export default function Home() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
-  );
-}
+export default Home;
